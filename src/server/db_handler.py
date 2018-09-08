7339940c7c5d003fd020b803646644082a4af49b
Daniele <daniseas98@gmail.com>
2018-09-08 19:02:41 +0200

    [Add]defined basic orm classes for db

--- src/server/db_handler.py
+++ src/server/db_handler.py
@@ -1,11 +1,39 @@
 import re
+from peewee import Model, IntegerField, CharField, PostgresqlDatabase
+
 file = open("./settings/postgres_settings.txt")
 
 #retrieving infos from te settings text file
 
-user = re.split(':|\n',file.readline())[1]
-password = re.split(':|\n',file.readline())[1]
-host = re.split(':|\n',file.readline())[1]
-port = re.split(':|\n',file.readline())[1]
+USER = re.split(':|\n',file.readline())[1]
+PASSWORD = re.split(':|\n',file.readline())[1]
+HOST = re.split(':|\n',file.readline())[1]
+PORT = re.split(':|\n',file.readline())[1]
 
+DB = PostgresqlDatabase(
+    "marinet_db",
+    user=USER,
+    password=PASSWORD,
+    host=HOST,
+    port=PORT
+)
+
+
+class BasicModel(Model):
+    class Meta:
+        database = DB
+
+class User(BasicModel):
+    id = IntegerField()
+    username = CharField()
+
+class UserFollows(BasicModel):
+    id = IntegerField()
+    follower_id = IntegerField()
+    followed_id = IntegerField()
+
+class Comments(BasicModel):
+    id = IntegerField()
+    comment = CharField()
+    user_id = IntegerField
 
