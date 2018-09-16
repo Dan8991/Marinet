ccbdacf6d8834855c4c153b7c2df40e32d59e031
Daniele <daniseas98@gmail.com>
2018-09-16 16:03:53 +0200

    [Add]button that submits the comment

--- src/client/components/basic_comp/CommentsInput.tsx
+++ src/client/components/basic_comp/CommentsInput.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 const textAreaStyle = {
     resize: "none" as "none",
     height:"90%",
-    width:"50%",
+    width:"100%",
     boxShadow:"3px 3px 2px grey",
     fontFamily:'"Times New Roman", Times, serif',
     fontSize:"18px",
--- src/client/components/pages/MarinetPage.tsx
+++ src/client/components/pages/MarinetPage.tsx
@@ -1,15 +1,29 @@
 import * as React from "react";
 import { CommentsInput } from "../basic_comp/CommentsInput";
+import { Button } from "../basic_comp/Button";
 
 export interface IMarinetPageProps {
     isNewUser: boolean
 }
 
+// const commentsInStyle = {
+//     width:"100%",
+//     height:"80px",
+//     display: "flex",
+//     justifyContent: "center",
+// }
+
+const btnPosStyle = {
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"
+}
+
 const commentsInStyle = {
     width:"100%",
     height:"80px",
-    display: "flex",
-    justifyContent: "center",
+    display: "grid",
+    gridTemplateColumns:"20% 60% 10% 10%",
 }
 
 export class MarinetPage extends React.Component<IMarinetPageProps, {}> {
@@ -20,8 +34,12 @@ export class MarinetPage extends React.Component<IMarinetPageProps, {}> {
 
     render() {
         return(
-            <div style={commentsInStyle}> 
+            <div style={commentsInStyle}>
+                <div/>
                 <CommentsInput/>
+                <div style={btnPosStyle}>
+                    <Button onClick={()=>{}}/>
+                </div>
             </div>
         );
     }
