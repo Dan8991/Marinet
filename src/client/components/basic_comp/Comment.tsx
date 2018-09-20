b6bd9505401e898ecdcc8a36504521d5259b82f9
Daniele <daniseas98@gmail.com>
2018-09-20 16:46:17 +0200

    [Add]improved comments style

--- src/client/components/basic_comp/Comment.tsx
+++ src/client/components/basic_comp/Comment.tsx
@@ -5,8 +5,36 @@ export interface ICommentProps{
     comment:IComment;
 }
 
+const boxStyle = {
+    width: "50%",
+    display: "grid",
+    gridRowsTemplate:"auto auto",
+    backgroundColor: "#99ef43",
+    justifyContent: "left",
+    // boxShadow: "5px 3px grey",
+    margin: "15px 0 15px 0",
+    borderColor: "grey",
+    borderStyle: "outset",
+    borderRadius: "10px",
+    padding:"0px 20px 0px 20px"
+}
+
+const usernameStyle = {
+    fontFamily: "Arial",
+    color: "#087220",
+    fontSize: "28px",
+}
+
+const commentStyle = {
+    fontFamily: "Times New Roman",
+    fontSize: "20px",
+}
+
 export function Comment(props: ICommentProps){
     return(
-        <h1>{props.comment.username+":"+props.comment.comment}</h1>
+        <div style={boxStyle}>
+            <h1 style={usernameStyle}>{props.comment.username}</h1>
+            <h1 style={commentStyle}>{props.comment.comment}</h1>
+        </div>
     );
 }
